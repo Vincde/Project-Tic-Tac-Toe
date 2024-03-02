bbf7eedb5a284d40f603240e4c45a894262a0b8e
Vincde <145802178+Vincde@users.noreply.github.com>
2024-03-02 15:34:47 +0100

    //

---
+++ script.js
@@ -0,0 +1,20 @@
+const gameBoard = (function(){
+    const board = [];
+
+    const setPlayer1 = (name,age) =>{
+        return{
+            name,age
+        };
+    }
+
+    const setPlayer2 = (name,age) =>{
+        return{
+            name,age
+        };
+    }
+
+})();
+
+function updateScreen(){
+    
+}
