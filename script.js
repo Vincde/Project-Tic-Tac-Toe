dbd35bfcca4c18137fd775f43b4911c2ae1c3507
Vincde <145802178+Vincde@users.noreply.github.com>
2024-03-03 21:39:38 +0100

    //

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const gameBoard = (function(){
-    const board = [];
 
-    const setPlayer1 = (name,age) =>{
-        return{
-            name,age
-        };
+
+const Player = {
+
+    createPlayer : function(name,symbol){
+        return {name,symbol};
     }
 
-    const setPlayer2 = (name,age) =>{
-        return{
-            name,age
-        };
-    }
-
-})();
-
-function updateScreen(){
-    
 }
+
+
+const gameBoard = {
+    board : ['a','a','a','a','a','a','a','a','a'],
+    player1 : Player.createPlayer('mimmo','x'),
+    player2 : Player.createPlayer('aldo','y'),
+
+    displayBoard: function(){
+        //queryslector for the buttons i suppose
+
+        for(let i = 0; i < this.board.length; i++){
+            console.log(this.board[i]);
+            //SelectedButton.value = this.board[i]
+        }
+    },
+
+    playBoard : function() {
+        //querySelector.
+
+        let whoIsThePlayer = 'x';
+
+        //selector for buttons
+
+        //when ison
+        //selectedButton.textcontent = whoisThePyaer
+
+        //board[i].textContent = WhoIsThePlayer
+        
+        
+        
+        //whoIsThePlayer = 'O';
+
+        
+    }
+}
+
