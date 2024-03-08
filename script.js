a0f9d78e03845885a4dfe236cc191e4cc9caa9f9
Vincde <145802178+Vincde@users.noreply.github.com>
2024-03-08 01:52:55 +0100

    //

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 const gameBoard = function(){
 
-    const board = ['','','','','','','','',''];
+    const board = ['a','','','b','','','','',''];
     
+    const displayBoard = function(){
+        for(elem of board){
+            console.log(elem);
+        }
+    };
+
+    const modifyBoard = (symbol,position) =>{
+        board[position] = symbol;
+    }
 
 
-}
+    
+    return {displayBoard,modifyBoard};
+} // PLEASE NOTE : NOT IIFE SO I HAVE TO CREATE A VARIABLE TO USE THOSE INSIDE FUNCTIONS  
 
 
 const player = function(){
-    
-}
+
+    const createPlayer = (name,symbol) =>{
+        return {name,symbol};
+    };
+
+    return {createPlayer};
+} // PLEASE NOTE : NOT IIFE SO I HAVE TO CREATE A VARIABLE TO USE THOSE INSIDE FUNCTIONS
+
 
 
 const game = function(){
 
+    const playerC = player();
+
+    const player1 = playerC.createPlayer('mimmo','X');
+    const player2 = playerC.createPlayer('also','Z');
+
+    
 }
+
 
 
